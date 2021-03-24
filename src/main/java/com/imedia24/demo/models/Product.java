f9ab17e5cbda8c8bb57cb1fb23ac88b80de97357
Khalil <daoulat.khalil@gmail.com>
2021-03-24 21:54:01 +0100

    All models Product/Categorie

---
+++ src/main/java/com/imedia24/demo/models/Product.java
@@ -0,0 +1,37 @@
+package com.imedia24.demo.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nameProduct;
+    private double price;
+
+    @ManyToOne
+    private Categorie categorie;
+
+
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedDate;
+
+}
