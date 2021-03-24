de0be71976f2c9e0b0da72ddd7f635e226ac6173
Khalil <daoulat.khalil@gmail.com>
2021-03-24 22:09:28 +0100

    Add the initial files

---
+++ src/main/java/com/imedia24/demo/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.imedia24.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Product")
+public class ProductController {
+}
---
+++ src/main/java/com/imedia24/demo/models/Category.java
@@ -0,0 +1,26 @@
+package com.imedia24.demo.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nameCategorie;
+
+    @OneToMany
+    private List<Product> productList;
+}
